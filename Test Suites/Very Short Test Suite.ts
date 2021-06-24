e591ece501a63c76dc091a45aebbb9eabf2cea5b
huynguyen2908 <huynguyen@kms-technology.com>
2021-06-24 10:38:21 +0700

    Short test

---
+++ Test Suites/Very Short Test Suite.ts
@@ -0,0 +1,20 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>Very Short Test Suite</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>0</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>false</rerunImmediately>
+   <testSuiteGuid>754bbf96-2cc5-46c6-8455-ec1ae3325bdf</testSuiteGuid>
+   <testCaseLink>
+      <guid>88ed2561-da17-497f-b3de-4f511c730808</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Short Test</testCaseId>
+   </testCaseLink>
+</TestSuiteEntity>
